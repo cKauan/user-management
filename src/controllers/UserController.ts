ecb0497dbdfd9ebbd6b30235bf466cd8056d30b1
Carlos Kauãn <carloskauanmoreiradesousa@gmail.com>
2020-11-15 12:45:32 -0300

    setup cors

--- src/controllers/UserController.ts
+++ src/controllers/UserController.ts
@@ -1,6 +1,6 @@
 import { Request, Response } from 'express';
 
-export default class UserController {
+export default {
     index(_: Request, response: Response): void {
         response.json({ message: 'Hello world' });
     }
