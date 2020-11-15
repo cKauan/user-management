eb25c914fefb946dddee76f702d8166703ae16f3
Carlos Kauãn <carloskauanmoreiradesousa@gmail.com>
2020-11-15 12:45:11 -0300

    add cors

--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,11 @@
 import express from 'express';
+import routes from './routes';
+import cors from 'cors';
 const app = express();
 const port = process.env.PORT || 5500;
-app.get('/', (req, res) => {
-    return res.json({ message: 'Hello world' });
-});
+
+app.use(express.json())
+app.use(cors())
+app.use(routes);
 app.listen(port, () => console.log('Running on port', port));
+
