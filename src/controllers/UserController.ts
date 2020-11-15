87990d726f0c1ae0dfb5bd9d9eed0fa577e84403
Carlos Kauãn <carloskauanmoreiradesousa@gmail.com>
2020-11-15 13:10:05 -0300

    add create method

--- src/controllers/UserController.ts
+++ src/controllers/UserController.ts
@@ -1,7 +1,32 @@
 import { Request, Response } from 'express';
+import knex from '../database/connection';
+import * as Yup from 'yup';
 
 export default {
-    index(_: Request, response: Response): void {
-        response.json({ message: 'Hello world' });
-    }
-}
+    async index(_: Request, response: Response): Promise<Response> {
+        const users = await knex('users');
+        return response.json(users);
+    },
+    async create(request: Request, response: Response): Promise<Response> {
+        const { name, phone, bought, notes } = request.body;
+        const data = {
+            name,
+            phone,
+            bought,
+            notes,
+        };  
+        const schema = Yup.object().shape({
+            name: Yup.string().required(),
+            phone: Yup.string().required(),
+            bought: Yup.number().required(),
+            notes: Yup.string().required(),
+            
+        });
+        await schema.validate(data, {
+            abortEarly: false,
+        });
+
+        await knex('users').insert(data);
+        return response.status(201).json(data);
+    },
+};
