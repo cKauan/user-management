445a0671efaacb873c905c3ffc515ab4f2eda9dd
Carlos Kauãn <carloskauanmoreiradesousa@gmail.com>
2020-11-15 13:10:20 -0300

    add express-async-errors module

--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,13 @@
 import express from 'express';
+import 'express-async-errors';
 import routes from './routes';
+import errorsHandler from './errors/handler';
 import cors from 'cors';
 const app = express();
 const port = process.env.PORT || 5500;
 
-app.use(express.json())
-app.use(cors())
+app.use(express.json());
+app.use(cors());
 app.use(routes);
+app.use(errorsHandler);
 app.listen(port, () => console.log('Running on port', port));
-
