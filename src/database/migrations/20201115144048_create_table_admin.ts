ff256bd8d6b1a4e8017447ecbca0e402a93136a9
Carlos Kauãn <carloskauanmoreiradesousa@gmail.com>
2020-11-15 15:27:45 -0300

    add table admin and uuid column

---
+++ src/database/migrations/20201115144048_create_table_admin.ts
@@ -0,0 +1,19 @@
+import * as Knex from "knex";
+
+
+export async function up(knex: Knex): Promise<void> {
+    await knex.raw('create extension if not exists "uuid-ossp"'),
+    await knex.schema.createTable('admin', ( table) => {
+        table.uuid('id').primary().unique().defaultTo(knex.raw('uuid_generate_v4()')),
+        table.text('name').notNullable(),
+        table.text('email').unique().notNullable(),
+        table.text('password').notNullable()
+    })
+}
+
+
+export async function down(knex: Knex): Promise<void> {
+    await knex.raw('drop extension if exists "uuid-ossp"'),
+    await knex.schema.dropTable('admin')
+    
+}
