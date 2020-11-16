dc23f0ade0088cd1e854274ef418c86a679c95e2
Carlos Kauãn <carloskauanmoreiradesousa@gmail.com>
2020-11-16 11:22:50 -0300

    add a user seed

--- src/database/seeds/001_user.ts
+++ src/database/seeds/001_user.ts
@@ -3,7 +3,7 @@ import * as Knex from 'knex';
 export async function seed(knex: Knex): Promise<void> {
     // Deletes ALL existing entries
     await knex('users').del();
-        
+
     // Inserts seed entries
     await knex('users').insert([
         {
