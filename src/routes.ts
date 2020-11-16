a1ffb3a8ce84bfb1f4e2dcd2e8569e9677a592b4
Carlos Kauãn <carloskauanmoreiradesousa@gmail.com>
2020-11-16 11:23:06 -0300

    add routes for superusers

--- src/routes.ts
+++ src/routes.ts
@@ -1,10 +1,14 @@
 import { Router } from 'express';
+import AdminController from './controllers/AdminController';
 import UserController from './controllers/UserController';
+import isAuthenticated from './middlewares/isAuthenticated';
 const routes = Router();
 
-routes.get('/users', UserController.index)
-routes.post('/users', UserController.create)
-routes.put('/user/:id', UserController.update)
-routes.delete('/user/:id', UserController.delete)
+routes.get('/users', isAuthenticated, UserController.index);
+routes.post('/users', UserController.create);
+routes.put('/user/:id', UserController.update);
+routes.delete('/user/:id', UserController.delete);
 
+routes.post('/sign', AdminController.sign);
+routes.get('/login', AdminController.login);
 export default routes;
