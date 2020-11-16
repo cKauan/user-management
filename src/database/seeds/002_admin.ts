49689e0fcd72b84f85b7b5ddb9fb8c08ee53ea7d
Carlos Kauãn <carloskauanmoreiradesousa@gmail.com>
2020-11-16 13:45:25 -0300

    add a default superuser   - name: Carlos Kauãn   - email: carloskauanmoreiradesousa@gmail.com   - password: carlos123

---
+++ src/database/seeds/002_admin.ts
@@ -0,0 +1,15 @@
+import * as Knex from 'knex';
+
+export async function seed(knex: Knex): Promise<void> {
+    // Deletes ALL existing entries
+    await knex('admin').del();
+
+    // Inserts seed entries
+    await knex('admin').insert([
+        {
+            name: 'Carlos Kauãn',
+            email: 'carloskauanmoreiradesousa@gmail.com',
+            password: 'carlos123',
+        },
+    ]);
+}
