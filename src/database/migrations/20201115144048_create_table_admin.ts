4d96eb205d1699c9901668ddf2fda1c2544f90b7
Carlos Kauãn <carloskauanmoreiradesousa@gmail.com>
2020-11-16 11:22:34 -0300

    add table admin for superusers

--- src/database/migrations/20201115144048_create_table_admin.ts
+++ src/database/migrations/20201115144048_create_table_admin.ts
@@ -1,19 +1,20 @@
-import * as Knex from "knex";
-
+import * as Knex from 'knex';
 
 export async function up(knex: Knex): Promise<void> {
     await knex.raw('create extension if not exists "uuid-ossp"'),
-    await knex.schema.createTable('admin', ( table) => {
-        table.uuid('id').primary().unique().defaultTo(knex.raw('uuid_generate_v4()')),
-        table.text('name').notNullable(),
-        table.text('email').unique().notNullable(),
-        table.text('password').notNullable()
-    })
+        await knex.schema.createTable('admin', (table) => {
+            table
+                .uuid('id')
+                .primary()
+                .unique()
+                .defaultTo(knex.raw('uuid_generate_v4()')),
+                table.text('name').notNullable(),
+                table.text('email').unique().notNullable(),
+                table.text('password').notNullable();
+        });
 }
-
 
 export async function down(knex: Knex): Promise<void> {
     await knex.raw('drop extension if exists "uuid-ossp"'),
-    await knex.schema.dropTable('admin')
-    
+        await knex.schema.dropTable('admin');
 }
