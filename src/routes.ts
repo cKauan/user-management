3b9bd1c68e762c4c94cc3e6a9bc5239f90bb7504
Carlos Kauãn <carloskauanmoreiradesousa@gmail.com>
2020-11-16 13:42:40 -0300

    change name sign to create

--- src/routes.ts
+++ src/routes.ts
@@ -10,6 +10,6 @@ routes.post('/users', isAuthenticated, UserController.create);
 routes.put('/user/:id', isAuthenticated, UserController.update);
 routes.delete('/user/:id', isAuthenticated, UserController.delete);
 
-routes.post('/sign', isSuperUser, AdminController.sign);
+routes.post('/sign', isSuperUser, AdminController.create);
 routes.get('/login', AdminController.login);
 export default routes;
