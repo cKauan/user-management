1e7e8e6691efb36e26ad6acf456a96aaf607fcd6
Carlos Kauãn <carloskauanmoreiradesousa@gmail.com>
2020-11-16 11:19:28 -0300

    add error handler   - add error response to some methods

--- src/errors/handler.ts
+++ src/errors/handler.ts
@@ -5,16 +5,38 @@ interface ValidationErrors {
     [key: string]: string[];
 }
 const errorHandler: ErrorRequestHandler = (error, req, res, _) => {
-    console.error(error);
+    console.log(error);
+    const [errorType] = error.toString().split(': ');
     if (error instanceof ValidationError) {
         const errors: ValidationErrors = {};
         error.inner.forEach((err) => {
             errors[err.path] = err.errors;
         });
 
-        return res.status(400).json({ message: 'Validation fails', errors });
+        return res.status(400).json({ message: 'Validation Fails', errors });
     }
-    return res.status(500).json({ message: 'Internal Server Error ' });
+    else if (error.code == 23505) {
+        return res
+            .status(400)
+            .json({ message: 'Validation Fails', error: error.detail });
+    }
+
+    switch (errorType) {
+        case 'JsonWebTokenError':
+            return res.status(401).json({
+                message: 'Authentication Fails',
+                error: 'Invalid Credentials',
+            });
+        case 'TokenExpiredError':
+            return res.status(401).json({
+                message: 'Authentication Fails',
+                error: 'Token Expired',
+            });
+        default:
+            break;
+    }
+
+    return res.status(500).json({ message: 'Internal Server Error' });
 };
 
 export default errorHandler;
